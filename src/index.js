002149844848c7315fc149b64da61af1e12b1259
sadnessOjisan <sadness.ojisan@gmail.com>
2023-01-21 22:03:59 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const ViewArea = ({ count }) => {
+  return <div>{count}</div>;
+};
+
+const Button = ({ onClick }) => {
+  return <button onClick={onClick}>click me</button>;
+};
+
+const App = () => {
+  const [state, setState] = React.useState(0);
+
+  return (
+    <div>
+      <ViewArea count={state} />
+      <Button
+        onClick={() => {
+          setState(state + 1);
+        }}
+      />
+    </div>
+  );
+};
+
 const domContainer = document.getElementById("body");
 const root = ReactDOM.createRoot(domContainer);
-root.render(React.createElement("p", {}, "a"));
+root.render(React.createElement(App));
